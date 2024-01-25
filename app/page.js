67383d52a326f58111e591733fc00162f8347198
awooooool <45804108+awooooool@users.noreply.github.com>
2024-01-25 13:00:54 +0700

    style(prettier): format using prettier

--- app/page.js
+++ app/page.js
@@ -1,55 +1,73 @@
-import { formatTime, getItem, getLatest } from './utils'
-export const revalidate = 3600 // revalidate the data at most every hour
- 
-export default async function Home({items}) {
+import { formatTime, getItem, getLatest } from './utils';
+export const revalidate = 3600; // revalidate the data at most every hour
 
-    const times = await getItem()
+export default async function Home({ items }) {
+  const times = await getItem();
 
-    const getLatest = (times)=> {
-        if (times && times.length > 0) {
-            const mostRecentUpdate = times.reduce((latest, item) => {
-                const currentUpdateTime = new Date(item.updatedAt);
-                return currentUpdateTime > latest ? currentUpdateTime : latest;
-            }, new Date(0)); // Initialize with the earliest possible date
-        
-            const formattedMostRecentUpdate = mostRecentUpdate.toLocaleString();
-            return formattedMostRecentUpdate;
-      }
+  const getLatest = (times) => {
+    if (times && times.length > 0) {
+      const mostRecentUpdate = times.reduce((latest, item) => {
+        const currentUpdateTime = new Date(item.updatedAt);
+        return currentUpdateTime > latest ? currentUpdateTime : latest;
+      }, new Date(0)); // Initialize with the earliest possible date
+
+      const formattedMostRecentUpdate = mostRecentUpdate.toLocaleString();
+      return formattedMostRecentUpdate;
     }
-      
+  };
+
   return (
-    <div style={{ backgroundImage: `url('/begobg.svg')` }} className="font-sans bg-cover py-10 px-24 h-screen">
-<div className="py-20 px-60 ">
-    <h2 className="text-white">Time Results</h2>
-    <h4 className="text-gray-300">Current as of {getLatest(times)}</h4>
-    <table className="w-full border-collapse my-12 text-sm bg-gray-200 text-left">
-        <thead>
+    <div
+      style={{ backgroundImage: `url('/begobg.svg')` }}
+      className='h-screen bg-cover px-24 py-10 font-sans'
+    >
+      <div className='px-60 py-20 '>
+        <h2 className='text-white'>Time Results</h2>
+        <h4 className='text-gray-300'>Current as of {getLatest(times)}</h4>
+        <table className='my-12 w-full border-collapse bg-gray-200 text-left text-sm'>
+          <thead>
             <tr>
-                <th className="w-11 px-4 py-2 text-center border-t-2 border-red-600 bg-gray-800 text-white">Rank</th>
-                <th className="px-4 py-2 text-center border-t-2 border-red-600 bg-gray-800 text-white">Driver</th>
-                <th className="px-4 py-2 text-center border-t-2 border-red-600 bg-gray-800 text-white">Time</th>
-                <th className="px-4 py-2 text-center border-t-2 border-red-600 bg-gray-800 text-white">Car Type</th>
-                <th className="px-4 py-2 text-center border-t-2 border-red-600 bg-gray-800 text-white">Gap</th>
+              <th className='w-11 border-t-2 border-red-600 bg-gray-800 px-4 py-2 text-center text-white'>
+                Rank
+              </th>
+              <th className='border-t-2 border-red-600 bg-gray-800 px-4 py-2 text-center text-white'>
+                Driver
+              </th>
+              <th className='border-t-2 border-red-600 bg-gray-800 px-4 py-2 text-center text-white'>
+                Time
+              </th>
+              <th className='border-t-2 border-red-600 bg-gray-800 px-4 py-2 text-center text-white'>
+                Car Type
+              </th>
+              <th className='border-t-2 border-red-600 bg-gray-800 px-4 py-2 text-center text-white'>
+                Gap
+              </th>
             </tr>
-        </thead>
-        <tbody>
-            {times.map((item,index)=>{
-                return(
-                    <tr className="bg-gray-600 text-white">
-                    <td className="px-4 py-2 text-center border-b border-gray-300">{index+1}</td>
-                    <td className="px-4 py-2 text-center border-b border-gray-300">{item.driver}</td>
-                    <td className="px-4 py-2 text-center border-b border-gray-300">{formatTime(item.best_lap)}</td>
-                    <td className="px-4 py-2 text-center border-b border-gray-300">{item.car}</td>
+          </thead>
+          <tbody>
+            {times.map((item, index) => {
+              return (
+                <tr key={index} className='bg-gray-600 text-white'>
+                  <td className='border-b border-gray-300 px-4 py-2 text-center'>
+                    {index + 1}
+                  </td>
+                  <td className='border-b border-gray-300 px-4 py-2 text-center'>
+                    {item.driver}
+                  </td>
+                  <td className='border-b border-gray-300 px-4 py-2 text-center'>
+                    {formatTime(item.best_lap)}
+                  </td>
+                  <td className='border-b border-gray-300 px-4 py-2 text-center'>
+                    {item.car}
+                  </td>
 
-                    <td className="px-4 py-2 text-center border-b border-gray-300">{`+${item.gap}`}</td>
+                  <td className='border-b border-gray-300 px-4 py-2 text-center'>{`+${item.gap}`}</td>
                 </tr>
-                )
+              );
             })}
-
-
-        </tbody>
-    </table>
-</div>
-</div>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
--- app/utils.js
+++ app/utils.js
@@ -1,39 +1,43 @@
-import { cache } from 'react'
+import { cache } from 'react';
 import clientPromise from './lib/mongodb';
- 
+
 export const getItem = cache(async () => {
-    const client = await clientPromise;
-    const db = client.db('SimRacing_Results');
-    
-    // Query for the fastest lap
-    const fastestLapResult = await db.collection('Times')
+  const client = await clientPromise;
+  const db = client.db('SimRacing_Results');
+
+  // Query for the fastest lap
+  const fastestLapResult = await db
+    .collection('Times')
     .find({})
     .sort({ best_lap: 1 })
     .limit(1)
     .toArray();
-    const fastestLap = fastestLapResult[0].best_lap;
+  const fastestLap = fastestLapResult[0].best_lap;
 
-    const results = await db.collection('Times').find({}).sort({ best_lap: 1 }).toArray();
+  const results = await db
+    .collection('Times')
+    .find({})
+    .sort({ best_lap: 1 })
+    .toArray();
 
-    const resultsWithGap = results.map(result => {
-      const gap = result.best_lap - fastestLap;
-      return { ...result, gap: gap.toFixed(3) }; // gap rounded to 3 decimal places
+  const resultsWithGap = results.map((result) => {
+    const gap = result.best_lap - fastestLap;
+    return { ...result, gap: gap.toFixed(3) }; // gap rounded to 3 decimal places
   });
-  
-  return resultsWithGap;
-})
 
+  return resultsWithGap;
+});
 
 export function formatTime(timeInSeconds) {
   try {
     const minutes = Math.floor(timeInSeconds / 60);
     const seconds = Math.floor(timeInSeconds) % 60;
-    const milliseconds = Math.round((timeInSeconds - Math.floor(timeInSeconds)) * 1000);
-  
+    const milliseconds = Math.round(
+      (timeInSeconds - Math.floor(timeInSeconds)) * 1000
+    );
+
     return `${minutes}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(3, '0')}`;
-  
   } catch (error) {
-    return "-"
+    return '-';
   }
 }
-
